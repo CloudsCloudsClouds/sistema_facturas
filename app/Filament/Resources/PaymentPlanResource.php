baf7746e226cdf24b09e7090b39b6e2f668fe5c7
Daniel Bernal <danisebber1234@gmail.com>
2024-05-28 20:27:09 -0400

    Small fix

--- app/Filament/Resources/PaymentPlanResource.php
+++ app/Filament/Resources/PaymentPlanResource.php
@@ -18,6 +18,7 @@ class PaymentPlanResource extends Resource
     protected static ?string $model = PaymentPlan::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationLabel = 'Planes de pago';
 
     public static function form(Form $form): Form
     {
